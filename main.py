f0c41d0c3b93989b9287fb940ea5a6d4abb3f523
BrivaHamisi <brivahamisi@gmail.com>
2025-04-14 22:32:31 +0300

    Fast API Tutorial

--- main.py
+++ main.py
@@ -28,5 +28,22 @@ async def get_user(user_id: str):
     return {"user_id": user_id}
 
 
-cla
+class FoodEnum(str, Enum):
+    fruits = "fruits"
+    vegetables = "vegetables"
+    dairy = "dairy"
 
+
+@app.get("/foods/{food_name}")
+async def get_food(food_name: FoodEnum):
+    if food_name == FoodEnum.vegetables:
+        return {"food_name": food_name, "Message": "You are healthy!"}
+
+    if food_name.value == 'fruits':
+        return {"food_name": food_name, "Message": "You are still healthy but like sweet things!"}
+
+    return {"food_name": food_name, "Message": "You are not healthy!"}
+
+@app.get("/items")
+async def list_items():
+    return {"items": []}
